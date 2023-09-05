6abbc7e40b85a7afb2f2777a2e1f6055753e71fc
Ashish8532 <ashishraj916263@outlook.com>
2023-09-05 18:40:18 +0530

    Implement user login & logout

--- LegalGen.API/Controllers/UserController.cs
+++ LegalGen.API/Controllers/UserController.cs
@@ -2,6 +2,7 @@
 using LegalGen.Domain.Dtos;
 using LegalGen.Domain.Helper;
 using LegalGen.Domain.Interfaces;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
@@ -14,14 +15,17 @@ namespace LegalGen.API.Controllers
     public class UserController : ControllerBase
     {
         private readonly IUserService _userService;
+        private readonly IConfiguration _configuration;
 
         /// <summary>
         /// Initializes a new instance of the <see cref="UserController"/> class.
         /// </summary>
         /// <param name="userService">The repository for user-related operations.</param>
-        public UserController(IUserService userService)
+        /// <param name="configuration">The configuration object used for accessing application settings.</param>
+        public UserController(IUserService userService, IConfiguration configuration)
         {
             _userService = userService;
+            _configuration = configuration;
         }
 
         /// <summary>
@@ -76,6 +80,69 @@ namespace LegalGen.API.Controllers
             }
 
             return Ok(new Response { Status = "Success", Message = "User registered successfully!" });
+        }
+
+
+        /// <summary>
+        /// Logs in a user and generates a JWT token upon successful authentication.
+        /// </summary>
+        /// <param name="model">The login dto containing user credentials.</param>
+        /// <returns>
+        /// An HTTP response containing the JWT token and its expiration time if login is successful,
+        /// or an error message if authentication fails.
+        /// </returns>
+        [HttpPost("login")]
+        public async Task<IActionResult> Login([FromBody] LoginDto model)
+        {
+            if (!ModelState.IsValid)
+            {
+                return StatusCode(StatusCodes.Status400BadRequest,
+                    new Response { Status = "Error", Message = "Invalid user credentials." });
+            }
+
+            //Check User
+            var user = await _userService.GetUserByEmailAsync(model.Email);
+            if (user == null)
+            {
+                return StatusCode(StatusCodes.Status400BadRequest,
+                    new Response { Status = "Error", Message = "User not found!" });
+            }
+            //Check Password
+            var result = await _userService.CheckPasswordAsync(user, model.Password);
+
+            if (result.Succeeded)
+            {
+                var jwtToken = _userService.GenerateJwtToken(user);
+
+                // Returning the token along with other information
+                return Ok(new
+                {
+                    jwtToken,
+                    expiration = DateTime.UtcNow.AddMinutes(Convert.ToInt32(_configuration["JWT:LifetimeInMinutes"]))
+                });
+            }
+            else
+            {
+                return StatusCode(StatusCodes.Status401Unauthorized,
+                     new Response { Status = "Error", Message = "Invalid password." });
+            }
+        }
+
+
+        /// <summary>
+        /// Signs the user out of the application.
+        /// </summary>
+        /// <remarks>
+        /// This method clears the user's authentication status, effectively logging them out.
+        /// </remarks>
+        /// <returns>
+        /// A <see cref="Task"/> representing the asynchronous operation.
+        /// </returns>
+        [HttpPost("logout")]
+        public async Task<IActionResult> Logout()
+        {
+            await _userService.SignOutAsync();
+            return Ok(new Response { Status = "Success", Message = "Logged out successfully!" });
         }
     }
 }
--- LegalGen.API/Program.cs
+++ LegalGen.API/Program.cs
@@ -6,6 +6,7 @@ using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.IdentityModel.Tokens;
+using Microsoft.OpenApi.Models;
 using System;
 using System.Text;
 
@@ -16,7 +17,33 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+
+// Define and configure Swagger documentation settings for API.
+builder.Services.AddSwaggerGen(c =>
+{
+    c.SwaggerDoc("v1", new OpenApiInfo { Title = "LegalGen.API", Version = "v1" });
+    // Add JWT Authentication support in Swagger
+    var securityScheme = new OpenApiSecurityScheme
+    {
+        Name = "JWT Authentication",
+        Description = "Enter your JWT token",
+        In = ParameterLocation.Header,
+        Type = SecuritySchemeType.Http,
+        Scheme = "bearer", // Use "bearer" as the scheme for your token
+        BearerFormat = "JWT",
+        Reference = new OpenApiReference
+        {
+            Type = ReferenceType.SecurityScheme,
+            Id = "Bearer"
+        }
+    };
+    c.AddSecurityDefinition("Bearer", securityScheme);
+    c.AddSecurityRequirement(new OpenApiSecurityRequirement
+        {
+            { securityScheme, new string[] { } }
+        });
+});
+
 
 // Database connection string configuration
 var connectionStrings = builder.Configuration.GetConnectionString("LegalGenAiEntities");
